e2baf6ef3d8cdce4d565d71633af426796b1920b
VFMR <reichv@ifo.de>
2022-09-25 10:34:11 +0200

    proper setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 from setuptools import setup
-import pathlib
 
-MYDIR = pathlib.Path(__file__).parent
-def read_file(input):
-    with open(input, 'r') as f:
-        content = f.read()
-    return content
-
-INSTALL_REQUIREMENTS = read_file(os.path.join(MYDIR, 'requirements.txt')).split()
-  
 
 setup(
     name='batchprocessing',
     version='0.1',
-    py_modules=['example'],
+    py_modules=['batchprocessing'],
     author='Valentin Reich',
     license='MIT',
-    install_requires=INSTALL_REQUIREMENTS,
+    install_requires=['tqdm', 'numpy', 'pandas']
         )
