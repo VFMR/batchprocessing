bb6fc3d215637d381835492212d2a36ddf0b9580
VFMR <reichv@ifo.de>
2022-11-24 12:18:12 +0100

    refactoring

--- batchprocessing.py
+++ batchprocessing.py
@@ -10,267 +10,219 @@ import numpy as np
 import pandas as pd
 
 
-def batch_predict(method):
-    @wraps(method)
-    def _wrapper(self, *args, **kwargs):
+class BatchProcessor:
+    def __init__(self,
+                 n_batches: int = 1,
+                 checkpoint_path: str = '',
+                 do_load_cp: bool = True,
+                 n_jobs: int = 1) -> None:
+        self._n_batches = n_batches
+        self._checkpoint_path =  checkpoint_path
+        self._do_load_cp = do_load_cp
+        self._n_jobs = n_jobs
 
-        checkpoint_path = _get_checkpoint_path(kwargs)
-        n_batches = _get_n_batches(kwargs)
-        n_jobs = _get_n_jobs(kwargs)
-        do_load_cp = _get_do_load_cp(kwargs)
+    def batch_predict(self, method):
+        @wraps(method)
+        def _wrapper(predictor_self, *args, **kwargs):
+            output = self._batch_predict_func(predictor_self, method, args, kwargs)
+            return output
+        return _wrapper
 
-        if n_batches is None or n_batches==1:
-            # no batch processing. Execute method normally.
-            output = method(self, *args, **kwargs)
+    def _batch_predict_func(self, predictor_self, method, args, kwargs):
+        if self._n_batches is None or self._n_batches == 1:
+            # execute normally
+            output = method(predictor_self, *args, **kwargs)
         else:
-            # batch processing
-            batches = _get_batches(kwargs, n_batches)
-            other_kwargs = {key: value for key, value in kwargs.items() if key!='X'}
+            # batch processing:
+            batches, first_iter = self._get_remaining_batches_and_iterator(kwargs)
+            self._check_makedir()
 
-            if do_load_cp:
-                _, last_iter = _check_load_checkpoints(checkpoint_path,
-                                                       other_kwargs,
-                                                       n_batches=n_batches)
-            else:
-                last_iter = None
-
-            if last_iter is None:
-                first_iter = 0
-            else:
-                first_iter = last_iter + 1
-                batches = _get_unprocessed_batches(last_iter, batches)
-
-            _check_makedir(checkpoint_path)
+            last_iter = self._get_last_iter(kwargs)
+            other_kwargs = self._get_other_kwargs(kwargs)
 
             # execute function for each batch
-            if n_jobs is not None and n_jobs>1:
-                Parallel(n_jobs=n_jobs)(
-                    delayed(_iterfunc)(
-                        self=self,
+            if self._n_jobs is not None and self._n_jobs>1:
+                Parallel(n_jobs=self._n_jobs)(
+                    delayed(self._iterfunc)(
+                        predictor_self=predictor_self,
                         x=x,
                         method=method,
                         args=args,
                         other_kwargs=other_kwargs,
-                        checkpoint_path=checkpoint_path,
                         i=i+first_iter,
-                        n_batches=n_batches
                     ) for i, x in enumerate(tqdm(batches)))
             else:
                 for i, x in enumerate(tqdm(batches)):
-                    _iterfunc(self=self, x=x, method=method, args=args,
-                              other_kwargs=other_kwargs,
-                              checkpoint_path=checkpoint_path,
-                              i=i+first_iter,
-                              n_batches=n_batches)
+                    self._iterfunc(predictor_self=predictor_self,
+                                    x=x,
+                                    method=method,
+                                    args=args,
+                                    other_kwargs=other_kwargs,
+                                    i=i+first_iter,
+                                    )
 
             # combine individual batch results into one matrix
             # TODO: implement a way to combine individual results when function returns multiple values (tuple)
-            results, last_iter = _check_load_checkpoints(checkpoint_path,
-                                                        other_kwargs,
-                                                        n_batches=n_batches)
-            output = pd.concat(results,
-                               axis=0,
-                               ignore_index=True)
+            results = self._load_result_checkpoints(last_iter=last_iter)
+            output = pd.concat(results, axis=0, ignore_index=True)
 
-            _cleanup_checkpoints(checkpoint_path)
+            self._cleanup_checkpoints()
 
         return output
 
-    return _wrapper
+    def _get_remaining_batches_and_iterator(self, kwargs):
+        batches = self._get_batches(kwargs, n_batches=self._n_batches)
+        other_kwargs = {key: value for key, value in kwargs.items() if key!='X'}
 
-
-def _iterfunc(self, x, method, args, other_kwargs, checkpoint_path, i, n_batches):
-    iter_output = method(self, *args, X=x, **other_kwargs)
-    # results.append(iter_output)
-    _save_checkpoints(checkpoint_path,
-                        iteration=i,
-                        df=iter_output,
-                        parameter_dict=other_kwargs,
-                        n_batches=n_batches)
-    # return iter_output
-
-
-def _get_unprocessed_batches(last_iter, batches):
-    new_batches = batches[last_iter+1:]
-    # for i, x in enumerate(batches):
-    #     if i <= last_iter and last_iter > 0:
-    #         continue
-    #     else:
-    #         new_batches.append(x)
-    return new_batches
-
-
-def _get_batches(kwargs: dict, n_batches: int) -> np.ndarray:
-    data = kwargs['X']
-    batches = np.array_split(data, n_batches)
-    return batches
-
-
-def _get_default_value(kwargs, param, default):
-    x = kwargs.get(param)
-    if x is None:
-        x = default
-    return x
-
-def _get_do_load_cp(kwargs: dict, default: bool = True) -> bool:
-    """Retrieve load-checkpoint flag from passed keyword dict.
-
-    Args:
-        kwargs (dict): Keywords arguments to the decorated function
-        default (bool): Default value. Defaults to True
-
-    Returns:
-        bool: Indicator for whether checkpoints shall be loaded
-    """
-    return _get_default_value(kwargs, 'do_load_checkpoint', default)
-
-
-def _get_checkpoint_path(kwargs: dict, default: str='') -> str:
-    """Retrieve checkpoint path from passed keyword dict
-
-    Example:
-        >>> _get_checkpoint_path({'checkpoint_path': '~/path/to/checkpoints/'})
-        '~/path/to/checkpoints/'
-        >>> _get_checkpoint_path({'A': 'B'})
-        ''
-
-    Args:
-        kwargs (dict): Keyword arguments to the decorated function
-        default (str): Default value. Defaults to ''
-
-    Returns:
-        str: String with the directory for checkpoints
-    """
-    return _get_default_value(kwargs, 'checkpoint_path', default)
-
-
-def _get_n_batches(kwargs: dict, default=1) -> int:
-    """Retrieve number of batches from passed keyword dict
-
-    Example:
-        >>> _get_n_batches({'n_batches': 5})
-        5
-        >>> _get_n_batches({'A': 10})
-
-    Args:
-        kwargs (dict): Keywords arguments to the decorated function.
-        default: value to resort to if n_batches has not been passed.
-
-    Returns:
-        int: Number of batches to split the data into and process.
-    """
-    return _get_default_value(kwargs, 'n_batches', default)
-
-
-def _get_n_jobs(kwargs: dict, default=1) -> int:
-    """Retrieve the number of parallel jobs to be run from passed keyword dict.
-
-    Args:
-        kwargs (dict): Keyword argumentds to the decorated function.
-        default (int, optional): value to resort to if n_jobs has not been passed.
-            Defaults to 1.
-
-    Returns:
-        int: Number of parallel processes.
-    """
-    return _get_default_value(kwargs, 'n_jobs', default)
-
-
-def _check_load_checkpoints(checkpoint_path: str=None,
-                           parameter_dict: dict=None,
-                           n_batches: int = None) -> Tuple[np.ndarray, int]:
-    if checkpoint_path is not None:
-        results, last_iter = _load_checkpoints(checkpoint_path,
-                                              parameter_dict=parameter_dict,
-                                              n_batches=n_batches)
-    else:
-        results = []
-        last_iter = None
-    return results, last_iter
-
-
-def _load_checkpoints(path_base: str,
-                      parameter_dict: dict = None,
-                      n_batches: int = None):
-    df_list = []
-
-    try:
-        with open(os.path.join(path_base, 'checkpoint.json'), 'r') as f:
-            checkpoint = json.loads(f.read())
-        if isinstance(checkpoint.get('last_iter'), int):
-            cp_found = True
+        if self._do_load_cp:
+            last_iter = self._get_last_iter(parameter_dict=other_kwargs)
         else:
-            print(f'Incompatible checkpoint value {checkpoint}')
-            print('Starting from beginning')
+            last_iter = None
+
+        if last_iter is None:
+            first_iter = 0
+        else:
+            first_iter = last_iter + 1
+            batches = self._get_unprocessed_batches(last_iter, batches)
+
+        return batches, first_iter
+
+    def _iterfunc(self,
+                  predictor_self,
+                  x,
+                  method,
+                  args,
+                  other_kwargs,
+                  i,
+                  ):
+        iter_output = method(predictor_self, *args, X=x, **other_kwargs)
+        self._save_checkpoints(iteration=i,
+                               df=iter_output,
+                               parameter_dict=other_kwargs)
+
+    @staticmethod
+    def _get_unprocessed_batches(last_iter, batches):
+        return batches[last_iter+1:]
+
+    @staticmethod
+    def _get_batches(kwargs: dict, n_batches: int) -> np.ndarray:
+        data = kwargs['X']
+        batches = np.array_split(data, n_batches)
+        return batches
+
+    @staticmethod
+    def _get_other_kwargs(kwargs):
+        return {key: value for key, value in kwargs.items() if key!='X'}
+
+    def _get_last_iter(self, parameter_dict):
+        if self._checkpoint_path is not None:
+            try:
+                with open(os.path.join(self._checkpoint_path, 'checkpoint.json'), 'r') as f:
+                    checkpoint = json.loads(f.read())
+                if isinstance(checkpoint.get('last_iter'), int):
+                    cp_found = True
+                else:
+                    print(f'Incompatible checkpoint value {checkpoint}')
+                    print('Starting from beginning')
+                    cp_found = False
+            except FileNotFoundError as e:
+                print(e)
+                print('Checkpoint file not found, starting from beginning')
+                cp_found = False
+        else:
             cp_found = False
-    except FileNotFoundError as e:
-        print(e)
-        print('Checkpoint file not found, starting from beginning')
-        cp_found = False
 
-    if cp_found:
-        # check if the different parameters have been used:
-        if parameter_dict is not None:
-            for key, value in parameter_dict.items():
-                if key in checkpoint.keys() and value != checkpoint[key]:
-                    raise ValueError(f'Attempting to continue with different parameters. Loaded value of {key} is {checkpoint[key]} but you passed {value}. Aborting. Manually delete the checkpoint directory or adjust the parameters to continue.')
+        if cp_found:
+            # check if the different parameters have been used:
+            if parameter_dict is not None:
+                for key, value in parameter_dict.items():
+                    if key in checkpoint.keys() and value != checkpoint[key]:
+                        raise ValueError(f'Attempting to continue with different parameters. Loaded value of {key} is {checkpoint[key]} but you passed {value}. Aborting. Manually delete the checkpoint directory or adjust the parameters to continue.')
+        else:
+            checkpoint = {'last_iter': None}
+            # if parameter_dict is not None:
+            #     checkpoint.update(parameter_dict)
 
-        for iteration in range(checkpoint['last_iter']+1):
-            padded_iteration = _get_padded_iterator(iteration, n_batches)
-            df_list.append(pd.read_csv(os.path.join(path_base, f'cp_{padded_iteration}.csv.gz'), index_col=0))
-    else:
-        checkpoint = {'last_iter': None}
+        return checkpoint['last_iter']
+
+    def _load_result_checkpoints(self, last_iter):
+        df_list = []
+
+        if last_iter is not None:
+            for iteration in range(last_iter+1):
+                padded_iteration = self._get_padded_iterator(iteration)
+                df_list.append(pd.read_csv(os.path.join(self._checkpoint_path, f'cp_{padded_iteration}.csv.gz'), index_col=0))
+
+        return df_list
+
+    def _get_padded_iterator(self, iteration: int):
+        if not isinstance(self._n_batches, int):
+            raise ValueError(f'Integer expected for the number of batches but received {type(self._n_batches)} instead.')
+        digits = len(str(self._n_batches))
+        iteration_padded = str(iteration).zfill(digits)
+        return iteration_padded
+
+    def _save_checkpoints(self,
+                        iteration: int,
+                        df: Union[pd.DataFrame, np.ndarray],
+                        parameter_dict: dict = None,
+                        ) -> None:
+
+        padded_iteration = self._get_padded_iterator(iteration)
+
+        checkpoint = {'last_iter': iteration}
         if parameter_dict is not None:
             checkpoint.update(parameter_dict)
 
-    return df_list, checkpoint['last_iter']
+        if not isinstance(df, pd.DataFrame):
+            df = pd.DataFrame(df)
+        df.to_csv(os.path.join(self._checkpoint_path, f'cp_{padded_iteration}.csv.gz'))
 
+        # export the checkpoint iterator last
+        with open(os.path.join(self._checkpoint_path, 'checkpoint.json'), 'w') as f:
+            f.write(json.dumps(checkpoint))
 
-def _get_padded_iterator(iteration: int, n_batches: int):
-    if not isinstance(n_batches, int):
-        raise ValueError(f'Integer expected for the number of batches but received {type(n_batches)} instead.')
-    digits = len(str(n_batches))
-    iteration_padded = str(iteration).zfill(digits)
-    return iteration_padded
+    def _check_makedir(self) -> None:
+        """Check if directory exists and creates it otherwise
 
+        Args:
+            path (str): Path to directory
+        """
+        if not os.path.isdir(self._checkpoint_path):
+            os.makedirs(self._checkpoint_path, exist_ok=True)
 
-def _save_checkpoints(path_base: str,
-                     iteration: int,
-                     df: Union[pd.DataFrame, np.ndarray],
-                     parameter_dict: dict = None,
-                     n_batches: int = None) -> None:
+    def _cleanup_checkpoints(self) -> None:
+        """Delete the checkpoint folder and all of its contents
 
-    padded_iteration = _get_padded_iterator(iteration, n_batches)
+        Args:
+            path (str): Checkpoint_folder
+        """
+        if os.path.isdir(self._checkpoint_path):
+            shutil.rmtree(self._checkpoint_path)
 
-    checkpoint = {'last_iter': iteration}
-    if parameter_dict is not None:
-        checkpoint.update(parameter_dict)
+    #---------------------------------------------------------------------------
+    # classmethods:
+    @classmethod
+    def batch_predict_auto(cls, method):
+        @wraps(method)
+        def _wrapper(predictor_self, *args, **kwargs):
+            checkpoint_path = kwargs.get('checkpoint_path')
+            n_batches = kwargs.get('n_batches')
+            n_jobs = kwargs.get('n_jobs')
+            do_load_cp = kwargs.get('do_load_cp')
+            instance = cls(checkpoint_path=checkpoint_path,
+                           n_batches=n_batches,
+                           n_jobs=n_jobs,
+                           do_load_cp=do_load_cp)
+            if checkpoint_path is not None:
+                instance._checkpoint_path = checkpoint_path
+            if n_batches is not None:
+                instance._n_batches = n_batches
+            if n_jobs is not None:
+                instance._n_jobs = n_jobs
+            if do_load_cp is not None:
+                instance._do_load_cp = do_load_cp
 
-    if not isinstance(df, pd.DataFrame):
-        df = pd.DataFrame(df)
-    df.to_csv(os.path.join(path_base, f'cp_{padded_iteration}.csv.gz'))
-
-    # export the checkpoint iterator last
-    with open(os.path.join(path_base, 'checkpoint.json'), 'w') as f:
-        f.write(json.dumps(checkpoint))
-
-
-def _check_makedir(path: str) -> None:
-    """Check if directory exists and creates it otherwise
-
-    Args:
-        path (str): Path to directory
-    """
-    if not os.path.isdir(path):
-        os.makedirs(path, exist_ok=True)
-
-
-def _cleanup_checkpoints(path: str) -> None:
-    """Delete the checkpoint folder and all of its contents
-
-    Args:
-        path (str): Checkpoint_folder
-    """
-    if os.path.isdir(path):
-        shutil.rmtree(path)
+            return instance._batch_predict_func(predictor_self, method, args, kwargs)
+        return _wrapper
 
--- tests/myunittests.py
+++ tests/myunittests.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+import time
 
 import numpy as np
 import pandas as pd
@@ -17,30 +18,46 @@ class MyMonkeyPatch:
         self.rnd_df.columns = self.colnames
         self.checkpoint_path=os.path.join(TEMPDIR, 'mytest')
         self.fake_cp_path = os.path.join(TEMPDIR, 'mytest_fake')
+        self._n_batches = 10
 
-    @batchprocessing.batch_predict
-    def add(self, X, n_batches=None, checkpoint_path=None):
+    @batchprocessing.BatchProcessor.batch_predict_auto
+    def add(self, X, n_batches=None, checkpoint_path=None, n_jobs=1):
+        time.sleep(.2)
         return X + 1
 
 
 class TestBatchProc(unittest.TestCase):
     def _setup(self):
         self.myobj = MyMonkeyPatch()
-        batchprocessing._check_makedir(self.myobj.fake_cp_path)
-        splits = np.array_split(self.myobj.rnd_df, 10)
+        self._n_batches = self.myobj._n_batches
+        splits = np.array_split(self.myobj.rnd_df, self._n_batches)
+        processor = batchprocessing.BatchProcessor(n_batches=self._n_batches,
+                                                   checkpoint_path=self.myobj.fake_cp_path)
+        processor._check_makedir()
+        # only save the first 5 iterations
         for i in range(5):
             splits_df = pd.DataFrame(splits[i])
-            batchprocessing._save_checkpoints(
-                self.myobj.fake_cp_path,
+            processor._save_checkpoints(
                 iteration=i,
-                df=splits_df
+                df=splits_df,
             )
 
     def test_batch_predict(self):
         self._setup()
         x = self.myobj.add(X=self.myobj.df,
-                           n_batches=10,
+                           n_batches=self._n_batches,
                            checkpoint_path=self.myobj.checkpoint_path)
+        assertion_df = pd.DataFrame(np.ones((100, 2)))
+        assertion_df.columns = self.myobj.colnames
+        assert x.shape == (100, 2)
+        assert x.equals(assertion_df)
+
+    def test_batch_predict_parallel(self):
+        self._setup()
+        x = self.myobj.add(X=self.myobj.df,
+                           n_batches=self._n_batches,
+                           checkpoint_path=self.myobj.checkpoint_path,
+                           n_jobs=4)
         assertion_df = pd.DataFrame(np.ones((100, 2)))
         assertion_df.columns = self.myobj.colnames
         assert x.shape == (100, 2)
@@ -48,19 +65,23 @@ class TestBatchProc(unittest.TestCase):
 
     def test_check_makedir(self):
         self._setup()
-        batchprocessing._check_makedir(self.myobj.checkpoint_path)
+        processor = batchprocessing.BatchProcessor(n_batches=self._n_batches,
+                                                   checkpoint_path=self.myobj.checkpoint_path)
+        processor._check_makedir()
         assert os.path.isdir(self.myobj.checkpoint_path)
 
     def test_cleanup_checkpoints(self):
         self._setup()
-        batchprocessing._check_makedir(self.myobj.checkpoint_path)
-        batchprocessing._cleanup_checkpoints(self.myobj.checkpoint_path)
+        processor = batchprocessing.BatchProcessor(n_batches=self._n_batches,
+                                                   checkpoint_path=self.myobj.checkpoint_path)
+        processor._check_makedir()
+        processor._cleanup_checkpoints()
         assert os.path.isdir(self.myobj.checkpoint_path) is False
 
     def test_load_checkpoints(self):
         self._setup()
         x = self.myobj.add(X=self.myobj.df,
-                           n_batches=10,
+                           n_batches=self._n_batches,
                            checkpoint_path=self.myobj.fake_cp_path)
         assertion_df1 = pd.DataFrame(np.ones((100, 2)))
         assertion_df1.columns = self.myobj.colnames
@@ -69,6 +90,9 @@ class TestBatchProc(unittest.TestCase):
             assertion_df1.iloc[50:,:]],
             axis=0, ignore_index=True
         )
+        print(x)
+        print()
+        print(assertion_df2)
         assert x.shape == (100, 2)
         assert x.equals(assertion_df1) is False
         assert np.allclose(x, assertion_df2, rtol=0.0001)
